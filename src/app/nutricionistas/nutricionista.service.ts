18f343b644fed8813fad7550869b9fccac5294f2
jmendez <mendezj374@gmail.com>
2021-11-23 21:46:15 -0500

    Cambio en Receta - Nutricionista

--- src/app/nutricionistas/nutricionista.service.ts
+++ src/app/nutricionistas/nutricionista.service.ts
@@ -12,3 +12,37 @@ export class NutricionistaService {
 
   getNutricionistas(): Observable<Nutricionista[]>{return of (NUTRICIONISTAS);}
 }
+
+/*
+export class UserService {
+  private url:string = 'http://localhost:8087/api/users'
+  private httpHeaders=new HttpHeaders({'Content-Type': 'application/json'})
+  constructor(private http:HttpClient) { }
+  getUsers():Observable<User[]>
+  {
+    return this.http.get(this.url).pipe(
+      map(response => response as User[])
+    );
+  }
+  create(user:User):Observable<User>{
+    return this.http.post<User>(
+      this.url,user,{headers: this.httpHeaders}
+    )
+  }
+  getUser(id):Observable<User>{
+    return this.http.get<User>(`${this.url}/${id}`);
+  }
+  update(user:User):Observable<User>{
+    return this.http.put<User>(
+      `${this.url}/${user.id}`,
+      user,
+      {headers: this.httpHeaders}
+    )
+  }
+  delete(id:number):Observable<User>{
+    return this.http.delete<User>(
+      `${this.url}/${id}`,
+      {headers: this.httpHeaders}
+    )
+  }
+}*/
